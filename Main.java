b4bb002963abb8275353fc50eb4af6e778450ab4
proba <varga9316@gmail.com>
2017-09-05 18:32:18 +0200

    hello_word

---
+++ Main.java
@@ -0,0 +1,18 @@
+/**
+ *
+ */
+package simple_project;
+/**
+ * @author Varga Adam
+ *
+ */
+public class Main {
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		int x = 0;
+		System.out.println("hello word");
+	}
+
+}
