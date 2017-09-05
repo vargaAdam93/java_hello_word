ea12c23d7bdd5ed1bae9d3fe5a114831b90c6314
proba <varga9316@gmail.com>
2017-09-05 18:34:20 +0200

    x inc

--- Main.java
+++ Main.java
@@ -2,16 +2,20 @@
  *
  */
 package simple_project;
+
 /**
  * @author Varga Adam
  *
  */
 public class Main {
+
 	/**
 	 * @param args
 	 */
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		int x = 0;
+		x ++;
 		System.out.println("hello word");
 	}
 
